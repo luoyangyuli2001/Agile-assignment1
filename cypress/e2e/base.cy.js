4b50ce082e0449b3106d5a4938e8e1d400c5d1d5
LuoYang <luoyangyuli2001@gmail.com>
2022-11-16 23:00:12 +0000

    Added a simple test to pass the pipeline.

---
+++ cypress/e2e/base.cy.js
@@ -0,0 +1,25 @@
+let movies;
+let movie;
+
+describe("Base tests", () => {
+  before(() => {
+    cy.request(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .its("body")
+      .then((response) => {
+        movies = response.results;
+      });
+  });
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  describe("The Homepage", () => {
+    it("displays the page header and 20 movies", () => {
+      cy.get("h3").contains("Discover Movies");
+      cy.get(".MuiCardHeader-root").should("have.length", 20);
+    });
+  });
+});
